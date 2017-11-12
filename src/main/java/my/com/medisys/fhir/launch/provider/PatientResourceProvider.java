af405a000a1bc4b9cac5e09e533886dadb6a7e22
Shahed Hossain <shahed@medisys.com.sa>
2017-11-12 17:48:57 +0600

    Optional Parameter added for Patient Search

--- src/main/java/my/com/medisys/fhir/launch/provider/PatientResourceProvider.java
+++ src/main/java/my/com/medisys/fhir/launch/provider/PatientResourceProvider.java
@@ -6,6 +6,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.api.ResourceMetadataKeyEnum;
 import ca.uhn.fhir.model.dstu2.composite.HumanNameDt;
@@ -20,6 +23,7 @@ import ca.uhn.fhir.model.primitive.UriDt;
 import ca.uhn.fhir.parser.DataFormatException;
 import ca.uhn.fhir.rest.annotation.Create;
 import ca.uhn.fhir.rest.annotation.IdParam;
+import ca.uhn.fhir.rest.annotation.OptionalParam;
 import ca.uhn.fhir.rest.annotation.Read;
 import ca.uhn.fhir.rest.annotation.RequiredParam;
 import ca.uhn.fhir.rest.annotation.ResourceParam;
@@ -36,7 +40,9 @@ import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
  * but it is useful to help illustrate how to build a fully-functional server.
  */
 public class PatientResourceProvider implements IResourceProvider {
-	
+    
+    private static final Logger log = LoggerFactory.getLogger(PatientResourceProvider.class);
+     
     /**
      * This map has a resource ID as a key, and each key maps to a Deque list containing all versions of the resource with that ID.
      */
@@ -139,13 +145,13 @@ public class PatientResourceProvider implements IResourceProvider {
      * The "@Search" annotation indicates that this method supports the search operation. You may have many different method annotated with this annotation, to support many different search criteria.
      * This example searches by family name.
      * 
-     * @param theFamilyName
+     * @param familyName
      *            This operation takes one parameter which is the search criteria. It is annotated with the "@Required" annotation. This annotation takes one argument, a string containing the name of
      *            the search criteria. The datatype here is StringDt, but there are other possible parameter types depending on the specific search criteria.
      * @return This method returns a list of Patients. This list may contain multiple matching resources, or it may also be empty.
      */
     @Search()
-    public List<Patient> findPatientsByName(@RequiredParam(name = Patient.SP_FAMILY) StringDt theFamilyName) {
+    public List<Patient> findPatientsByName(@RequiredParam(name = Patient.SP_FAMILY) StringDt familyName, @OptionalParam(name = Patient.SP_GIVEN) StringDt givenName) {
         LinkedList<Patient> retVal = new LinkedList<Patient>();
         
         /*
@@ -155,12 +161,16 @@ public class PatientResourceProvider implements IResourceProvider {
             Patient nextPatient = nextPatientList.getLast();
             NAMELOOP: for (HumanNameDt nextName : nextPatient.getName()) {
                 for (StringDt nextFamily : nextName.getFamily()) {
-                    if (theFamilyName.equals(nextFamily)) {
+                    if (familyName.equals(nextFamily)) {
                         retVal.add(nextPatient);
                         break NAMELOOP;
                     }
                 }
             }
+        }
+        log.info("Family Name: {}", familyName.getValueAsString());
+        if(givenName != null){
+            log.info("Given Name: {}", givenName.getValueAsString());
         }
         
         return retVal;
